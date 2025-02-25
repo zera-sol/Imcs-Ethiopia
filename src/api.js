29daa493237ed5ce013f5e4f61369f568cccde25
zera-sol <zedomanwithjesu1994@gmail.com>
2025-02-25 11:28:22 +0300

    commit day 15

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // the api to send request
 // http://localhost:5000/api
 // https://imcs-ethiopia-backend.onrender.com/api
-const api = "http://localhost:5000/api";
+const api = " https://imcs-ethiopia-backend.onrender.com/api";
 export default api;
