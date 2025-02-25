7d389d80651e4d718ca61a24294588a1041a93d0
zera-sol <zedomanwithjesu1994@gmail.com>
2025-02-25 13:49:42 +0300

    eyuel tesfaye

--- src/components/EditBlog.js
+++ src/components/EditBlog.js
@@ -8,12 +8,13 @@ const EditBlog = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { blog } = location.state || {};
-
+  const user = localStorage.getItem("user");
   const [title, setTitle] = useState(blog?.title || "");
   const [content, setContent] = useState(blog?.content || "");
   const [image, setImage] = useState(blog?.image || "");
   const [newImage, setNewImage] = useState(null); // For the new image preview
   const [isLoading, setIsLoading] = useState(false);
+
 
   // Function to handle image change
   const handleImageChange = (e) => {
@@ -73,64 +74,72 @@ const EditBlog = () => {
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 bg-[#3992CE]">
-      <h1 className="text-3xl font-bold mb-4 mt-4 text-center  text-white">Edit a News</h1>
-      <div className="bg-blue-50 rounded-lg shadow-md p-6 mx-auto max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-3xl">
-        {/* Image Preview Section */}
-        {/* Image Preview Section */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Current Image</label>
-          <div className="relative flex justify-center">
-            <img
-              src={image} // Shows either the existing or new preview
-              alt="Blog Image"
-              className="w-full sm:w-3/4 md:w-1/2 h-48 object-cover rounded-md"
-            />
-            <div className="absolute top-0 right-0 p-2 cursor-pointer">
-              <label htmlFor="image-upload" className="text-black rounded-full p-2">
-                <FaCamera size={20} />
-              </label>
-              <input
-                id="image-upload"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="hidden"
-              />
+      {user ? (
+        <>
+            <h1 className="text-3xl font-bold mb-4 mt-4 text-center  text-white">Edit a News</h1>
+            <div className="bg-blue-50 rounded-lg shadow-md p-6 mx-auto max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-3xl">
+              {/* Image Preview Section */}
+              {/* Image Preview Section */}
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Current Image</label>
+                <div className="relative flex justify-center">
+                  <img
+                    src={image} // Shows either the existing or new preview
+                    alt="Blog Image"
+                    className="w-full sm:w-3/4 md:w-1/2 h-48 object-cover rounded-md"
+                  />
+                  <div className="absolute top-0 right-0 p-2 cursor-pointer">
+                    <label htmlFor="image-upload" className="text-black rounded-full p-2">
+                      <FaCamera size={20} />
+                    </label>
+                    <input
+                      id="image-upload"
+                      type="file"
+                      accept="image/*"
+                      onChange={handleImageChange}
+                      className="hidden"
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* Title Input */}
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Title</label>
+                <textarea
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  rows="2"
+                />
+              </div>
+
+              {/* Content Input */}
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Content</label>
+                <textarea
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                  rows="4"
+                />
+              </div>
+
+              {/* Save Button */}
+              <div className="text-center">
+                <button
+                  onClick={handleSave}
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                >
+                  {isLoading ? "Loading..." : "Save Changes"}
+                </button>
+              </div>
             </div>
-          </div>
-        </div>
-        {/* Title Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Title</label>
-          <textarea
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            rows="2"
-          />
-        </div>
-
-        {/* Content Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Content</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            rows="4"
-          />
-        </div>
-
-        {/* Save Button */}
-        <div className="text-center">
-          <button
-            onClick={handleSave}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            {isLoading ? "Loading..." : "Save Changes"}
-          </button>
-        </div>
-      </div>
+          </>
+          ) : (
+            <div className="text-center mt-10 text-red-600 text-2xl font-bold">
+              You are not authorized to edit this news.
+            </div>
+          )}
     </div>
   );
 };
--- src/components/EditGraduate.js
+++ src/components/EditGraduate.js
@@ -6,8 +6,8 @@ import api from "../api"
 const EditGraduate = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { graduate } = location.state || {};
-
+  const { graduate } = location.state || {};  
+  const user = localStorage.getItem("user");
   const [name, setName] = useState(graduate?.name || "");
   const [university, setUniversity] = useState(graduate?.university || "");
   const [department, setDepartment] = useState(graduate?.department || "");
@@ -67,97 +67,105 @@ const EditGraduate = () => {
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 bg-[#3992CE]">
-      <h1 className="text-3xl font-bold mb-4 mt-4 text-center text-white">Edit Graduate</h1>
-      <div className="bg-blue-50 rounded-lg shadow-md p-6 mx-auto max-w-lg">
-        {/* Image Preview Section */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Current Image</label>
-          <div className="relative flex justify-center">
-            <img
-              src={image}
-              alt="Graduate Image"
-              className="w-full sm:w-3/4 md:w-1/2 h-48 object-cover rounded-md"
-            />
-            <div className="absolute top-0 right-0 p-2 cursor-pointer">
-              <label htmlFor="image-upload" className="text-black rounded-full p-2">
-                <FaCamera size={20} />
-              </label>
-              <input
-                id="image-upload"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="hidden"
-              />
-            </div>
-          </div>
-        </div>
-        {/* Name Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Name</label>
-          <input
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        {/* University Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">University</label>
-          <input
-            value={university}
-            onChange={(e) => setUniversity(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        {/* Department Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Department</label>
-          <input
-            value={department}
-            onChange={(e) => setDepartment(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        {/* Year Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Year</label>
-          <input
-            type="number"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        {/* Phone Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Phone</label>
-          <input
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        {/* Last Words Input */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">Last Words</label>
-          <textarea
-            value={lastword}
-            onChange={(e) => setLastWord(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            rows="3"
-          />
-        </div>
-        {/* Save Button */}
-        <div className="text-center">
-          <button
-            onClick={handleSave}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            {isLoading ? (<FaSpinner className="animate-spin text-white-500 text-4xl w-1/2 mx-auto"/>)  : "Save Changes"}
-          </button>
-        </div>
-      </div>
+        {user? (
+          <>
+                    <h1 className="text-3xl font-bold mb-4 mt-4 text-center text-white">Edit Graduate</h1>
+                    <div className="bg-blue-50 rounded-lg shadow-md p-6 mx-auto max-w-lg">
+                        {/* Image Preview Section */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Current Image</label>
+                        <div className="relative flex justify-center">
+                            <img
+                            src={image}
+                            alt="Graduate Image"
+                            className="w-full sm:w-3/4 md:w-1/2 h-48 object-cover rounded-md"
+                            />
+                            <div className="absolute top-0 right-0 p-2 cursor-pointer">
+                            <label htmlFor="image-upload" className="text-black rounded-full p-2">
+                                <FaCamera size={20} />
+                            </label>
+                            <input
+                                id="image-upload"
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                                className="hidden"
+                            />
+                            </div>
+                        </div>
+                        </div>
+                        {/* Name Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Name</label>
+                        <input
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                        />
+                        </div>
+                        {/* University Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">University</label>
+                        <input
+                            value={university}
+                            onChange={(e) => setUniversity(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                        />
+                        </div>
+                        {/* Department Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Department</label>
+                        <input
+                            value={department}
+                            onChange={(e) => setDepartment(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                        />
+                        </div>
+                        {/* Year Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Year</label>
+                        <input
+                            type="number"
+                            value={year}
+                            onChange={(e) => setYear(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                        />
+                        </div>
+                        {/* Phone Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Phone</label>
+                        <input
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                        />
+                        </div>
+                        {/* Last Words Input */}
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Last Words</label>
+                        <textarea
+                            value={lastword}
+                            onChange={(e) => setLastWord(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md"
+                            rows="3"
+                        />
+                        </div>
+                        {/* Save Button */}
+                        <div className="text-center">
+                        <button
+                            onClick={handleSave}
+                            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                        >
+                            {isLoading ? (<FaSpinner className="animate-spin text-white-500 text-4xl w-1/2 mx-auto"/>)  : "Save Changes"}
+                        </button>
+                        </div>
+                    </div> 
+                 </>
+                ) : (
+                    <div className="text-center mt-10 text-red-600 text-2xl font-bold">
+                    You are not authorized to edit this news.
+                    </div>
+                 )}
     </div>
   );
 };
